1daf3a8b1ffd9345aeac01ad62687a323ca768b4
dailylifeassysadmin <raviindia77@hotmail.com>
2024-08-30 12:59:44 +0530

    Fix: Update the script for ping check to list of servers.

--- 05-Ping_to_list_of_server.sh
+++ 05-Ping_to_list_of_server.sh
@@ -11,14 +11,14 @@ ServerArray=("www.google.com" "www.yahoo.in" "www.office.com" "www.ubuntu.com" "
 #echo $2, $1, $3, $4, $6, $7
 #echo ${ServerArray[0]}
 #echo ${ServerArray[@]}
-echo ${!ServerArray[@]}
-echo ${#ServerArray[@]}
+#echo ${!ServerArray[@]}
+#echo ${#ServerArray[@]}
 
 # now we are iterating one by one via a "for" loop with list method.
 #for i in $ServerList; do
 #    echo -e "$i\n"
 #done
-set -x
+#set -x
 
 # now we are iterating one by one via a "for" loop with array method.
 for i in ${ServerArray[@]}; do
@@ -26,16 +26,8 @@ for i in ${ServerArray[@]}; do
     #echo -e "\n"
     if [ $? -eq 0 ]; then
         echo "$i: Connected" >> $0.txt
-        for j in ${!ServerArray[@]}; do
-            if [[ $(expr $j+1) -eq ${#ServerArray[@]} ]]; then
-                echo "-------------------------" >> $0.txt
-            fi
-        done
     else
-        echo "$i: Not Connected" >> $0.error.txt
-        if [{ ${#ServerArray[@]} == ${#ServerArray[@]} }]; then
-            echo "-------------------------" >> $0.error.txt
-        fi
+        echo "$i: Not Connected" >> $0.txt
     fi
 done
 #if [ -f $0.txt ]; then
@@ -43,3 +35,11 @@ done
 #else
 #    echo "-------------------------" >> $0.error.txt
 #fi
+
+# Added this "for" loop and "if" condition to add a "END" line in the log file.
+# 
+for j in ${!ServerArray[@]}; do
+    if [[ $(expr $j+1) -eq ${#ServerArray[@]} ]]; then
+        echo "-------------END-------------" >> $0.txt
+    fi
+done
